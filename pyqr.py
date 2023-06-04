7e040fcd52e53b98766ca8b62723c3b55dbc393c
Parthiv <parthivanil2002@gmail.com>
2023-06-04 21:07:40 +0530

    updated

--- pyqr.py
+++ pyqr.py
@@ -1,7 +1,17 @@
 import pyqrcode
-import png
+import requests
 from pyqrcode import QRCode
-s = "www.google.com"
-url = pyqrcode.create(s)
-url.svg("myqr.svg", scale = 6)
-url.png('myqr.png', scale = 6)
+print("Enter link (with http://): ")
+s=input()
+try:
+        response = requests.get(s)
+        if response.status_code == 200:
+            url = pyqrcode.create(s)
+            url.svg("myqr.svg", scale=6)
+            url.png("myqr.png", scale=6)
+            print("QR code generated successfully!")
+        else:
+            print("Invalid or non-existent link. Please check the URL and try again.")
+except requests.exceptions.RequestException as e:
+    print("An error occurred while accessing the link:", str(e))
+
